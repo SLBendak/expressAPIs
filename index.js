6396dc9cbae524e7e774467dd1ffe4be5d96d9f5
mac <mac@macs-MacBook-Air.local>
2020-08-06 16:53:17 -0700

    [express] add API calls

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+let API_KEY = process.env.API_KEY
+// using .env to hide our API key
+
+app.set('view engine', 'ejs')
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    let qs = {
+        params: {
+            s: 'star wars',
+            apikey: API_KEY
+        }
+    }
+    axios.get('http://www.omdbapi.com', qs)
+    .then((response) => {
+        console.log(response.data)
+        let episodes = response.data.Search
+        // setting variable to our data
+        res.render('home', {episodes})
+        // render home with the data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+app.listen(3000);
